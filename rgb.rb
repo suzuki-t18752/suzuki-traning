e75496b644f5f09ca36298f340e19b65c545c2ac
suzuki-t18752 <52282236+suzuki-t18752@users.noreply.github.com>
2022-06-22 22:08:06 +0900

    Create rgb.rb

---
+++ rgb.rb
@@ -0,0 +1,14 @@
+def to_hex(r, g, b)
+  '#' + r.to_s(16).rjust(2, '0') + g.to_s(16).rjust(2, '0') + b.to_s(16).rjust(2, '0')
+end
+
+require 'minitest/autorun'
+require './rgb.rb'
+
+class RgbTest < Minitest::Test
+  def test_to_hex
+    assert_equal '#000000', to_hex(0, 0, 0)
+    assert_equal '#ffffff', to_hex(255, 255, 255)
+    assert_equal '#043c78', to_hex(4, 60, 120)
+  end
+end
